5aab8c72a930876c1c97d384d58a8451f85fa76d
ziwei531 <59821731+ziwei531@users.noreply.github.com>
2023-03-18 20:51:56 +0800

    update self hoist

--- bot/database.py
+++ bot/database.py
@@ -1,6 +1,5 @@
 from typing import Optional, Any
 
-import pymongo
 import uuid
 from datetime import datetime
 
@@ -9,20 +8,14 @@ import config
 
 class Database:
     def __init__(self):
-        self.client = pymongo.MongoClient(config.mongodb_uri)
-        self.db = self.client["chatgpt_telegram_bot"]
+        self.dialog_id = None
+        self.dialog_messages = []
+        self.user_collection = []
+        self.dialog_collection = []
 
-        self.user_collection = self.db["user"]
-        self.dialog_collection = self.db["dialog"]
 
     def check_if_user_exists(self, user_id: int, raise_exception: bool = False):
-        if self.user_collection.count_documents({"_id": user_id}) > 0:
-            return True
-        else:
-            if raise_exception:
-                raise ValueError(f"User {user_id} does not exist")
-            else:
-                return False
+        return self.find_one(self.user_collection, {"_id": user_id})
         
     def add_new_user(
         self,
@@ -50,8 +43,27 @@ class Database:
         }
 
         if not self.check_if_user_exists(user_id):
-            self.user_collection.insert_one(user_dict)
+            self.user_collection.append(user_dict)
 
+    def compare_dicts(self, dict_a, dict_b):
+        for key in dict_a:
+            if key not in dict_b or dict_a[key] != dict_b[key]:
+                return False
+        return True
+
+    def update_database(self, arr, condition, updated):
+        for elem in arr:
+            if self.compare_dicts(condition, elem):
+                elem.update(updated)
+                return arr
+        return arr
+
+    def find_one(self, arr, condition):
+        for elem in arr:
+            if self.compare_dicts(condition, elem):
+                return elem
+        return None
+        
     def start_new_dialog(self, user_id: int):
         self.check_if_user_exists(user_id, raise_exception=True)
 
@@ -65,37 +77,30 @@ class Database:
         }
 
         # add new dialog
-        self.dialog_collection.insert_one(dialog_dict)
+        self.dialog_collection.append(dialog_dict)
 
         # update user's current dialog
-        self.user_collection.update_one(
-            {"_id": user_id},
-            {"$set": {"current_dialog_id": dialog_id}}
-        )
-
+        self.update_database(self.user_collection,{"_id": user_id}, {"current_dialog_id": dialog_id} )
+        self.dialog_id = dialog_id
         return dialog_id
 
     def get_user_attribute(self, user_id: int, key: str):
         self.check_if_user_exists(user_id, raise_exception=True)
-        user_dict = self.user_collection.find_one({"_id": user_id})
-
+        user_dict = self.find_one(self.user_collection, {"_id": user_id})
         if key not in user_dict:
             raise ValueError(f"User {user_id} does not have a value for {key}")
-
         return user_dict[key]
 
     def set_user_attribute(self, user_id: int, key: str, value: Any):
         self.check_if_user_exists(user_id, raise_exception=True)
-        self.user_collection.update_one({"_id": user_id}, {"$set": {key: value}})
+        self.update_database(self.user_collection, {"_id": user_id}, {key: value})
 
     def get_dialog_messages(self, user_id: int, dialog_id: Optional[str] = None):
-        self.check_if_user_exists(user_id, raise_exception=True)
-
         if dialog_id is None:
             dialog_id = self.get_user_attribute(user_id, "current_dialog_id")
-
-        dialog_dict = self.dialog_collection.find_one({"_id": dialog_id, "user_id": user_id})               
+        dialog_dict = self.find_one(self.dialog_collection, {"_id": dialog_id, "user_id": user_id})               
         return dialog_dict["messages"]
+
 
     def set_dialog_messages(self, user_id: int, dialog_messages: list, dialog_id: Optional[str] = None):
         self.check_if_user_exists(user_id, raise_exception=True)
@@ -103,7 +108,7 @@ class Database:
         if dialog_id is None:
             dialog_id = self.get_user_attribute(user_id, "current_dialog_id")
         
-        self.dialog_collection.update_one(
+        self.update_database(self.dialog_collection,
             {"_id": dialog_id, "user_id": user_id},
-            {"$set": {"messages": dialog_messages}}
+            {"messages": dialog_messages}
         )
